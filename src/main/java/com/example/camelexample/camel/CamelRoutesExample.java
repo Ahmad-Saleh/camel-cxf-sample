3b79e25b4e81f1a7b10375e21754ee6da0357530
Ahmad Saleh <ahmad.saleh@progressoft.com>
2021-09-26 17:38:08 +0300

    sdfsdf

--- src/main/java/com/example/camelexample/camel/CamelRoutesExample.java
+++ src/main/java/com/example/camelexample/camel/CamelRoutesExample.java
@@ -1,12 +1,16 @@
 package com.example.camelexample.camel;
 
 import com.progressoft.corpay.oab.vasco.administration.AdminDomainList;
+import com.progressoft.corpay.oab.vasco.administration.UserAttribute;
 import com.progressoft.corpay.oab.vasco.administration.UserAttributeSet;
 import com.progressoft.corpay.oab.vasco.administration.UserCmdIDEnum;
+import com.progressoft.corpay.oab.vasco.administration.UserResults;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.cxf.message.MessageContentsList;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Element;
 
 import javax.jws.WebParam;
 import java.util.ArrayList;
@@ -33,6 +37,14 @@ public class CamelRoutesExample extends RouteBuilder {
                     exchange.getMessage().setBody(list);
                 })
                 .to("cxf:bean:adminittration?defaultOperationName=userExecute")
+                .process(exchange -> {
+                    UserResults userResults = exchange.getIn().getBody(UserResults.class);
+                    List<UserAttribute> attributes = userResults.getResultAttribute().getAttributes();
+                    for (UserAttribute userAttribute : attributes) {
+                        userAttribute.setValue(((Element) userAttribute.getValue()).getFirstChild().getNodeValue());
+                    }
+                })
+                .process(exchange -> System.out.println(exchange.getIn().getBody()))
                 .log("response: ${body}");
 
     }
